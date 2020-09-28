91874a0e1d0d19af47389e22e7b558975a18eaf9
markkizz <mark.pattanapara@gmail.com>
2020-09-28 10:09:56 +0700

    init vuetify and style loader

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/taskbox">Task Box Story book tutorial</router-link>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -28,5 +29,5 @@
       color: #42b983;
     }
   }
-}
+} */
 </style>
---
+++ src/views/taskbox/Taskbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Hello Taskbox
+  </div>
+</template>
+<script lang="ts">
+import {
+  Vue, Component, Prop, Watch,
+} from 'vue-property-decorator';
+import {
+  State, Getter, Action, Mutation, namespace,
+} from 'vuex-class';
+
+@Component
+export default class Taskbox extends Vue {
+
+}
+</script>
+<style lang="scss">
+@import './styles/index.scss';
+</style>
